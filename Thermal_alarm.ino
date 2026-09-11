b8460379f3c0a250ef86cab11fbac2b055f9f689
Hani <saidi7hani@gmail.com>
2026-09-11 15:10:18 +0100

    upload arduino code

---
+++ Thermal_alarm.ino
@@ -0,0 +1,56 @@
+#include<DHT.h>
+
+DHT dht(2,DHT11);
+float temperature;
+int currentbuttonstate;
+int alarmknowledged=LOW;
+int lastbuttonstate=HIGH;
+
+void setup() {
+  dht.begin();
+  Serial.begin(9600);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,INPUT_PULLUP);
+  delay(2000);
+}
+
+void alarm(){
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  tone(8,1500);
+}
+
+void toggle(){
+  alarmknowledged=!alarmknowledged;
+  digitalWrite(3,LOW);
+  digitalWrite(4,alarmknowledged);
+  noTone(8);
+}
+
+void loop() {
+  temperature=dht.readTemperature();
+  currentbuttonstate=digitalRead(7);
+  Serial.print(temperature);
+  Serial.print("-");
+  Serial.print(alarmknowledged);
+  Serial.print("-");
+  Serial.println(currentbuttonstate);
+
+  if(temperature<31){
+    digitalWrite(4,HIGH);
+    digitalWrite(3,LOW);
+    noTone(8);
+    alarmknowledged=LOW;
+  }
+
+  if(temperature>31 && alarmknowledged==LOW){
+    alarm();
+    delay(50);
+  }
+  if(currentbuttonstate==0 && lastbuttonstate==1 && temperature>31){
+    toggle();
+  }
+  lastbuttonstate=currentbuttonstate;
+}
